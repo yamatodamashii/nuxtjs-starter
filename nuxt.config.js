dfd1994abf416256a492df4d3a42d6f0195f4600
yamatodamashii <vj_yamato@yahoo.co.jp>
2020-11-03 10:27:23 +0900

    Update build config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import { projects } from "./utils/projectsData";
 
 export default {
   mode: 'universal',
@@ -50,6 +51,17 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+    }
+  },
+  generate: {
+    async routes() {
+      const paths = [];
+
+      projects.forEach(project => {
+        paths.push(`/project/${project.slug}`);
+      });
+
+      return paths;
     }
   }
 }
